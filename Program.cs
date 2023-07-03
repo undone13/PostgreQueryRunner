b1c50a2acfee5cdc0aeb2fcf1d49b534321826e6
DESKTOP-8NUOUMF\Bogdan <bogdanichim04@gmail.com>
2023-07-04 00:32:35 +0300

    Connect to PostgreSQL and execute all queries.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CSharp;
+﻿using Npgsql;
+using System.Text.Json;
+using System.Xml.Linq;
 
 namespace PostgreQueryRunner
 {
@@ -6,7 +8,56 @@ namespace PostgreQueryRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 1;
+
+            string textFile = System.IO.Directory.GetCurrentDirectory() + "\\input.json";
+
+            string text = File.ReadAllText(textFile);
+
+            var root = JsonSerializer.Deserialize<Root>(text);
+
+
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    root.appsettings.host,
+                    root.appsettings.username,
+                    root.appsettings.dbname,
+                    root.appsettings.port,
+                    root.appsettings.password);
+
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    Console.Out.WriteLine("Opening connection");
+                    conn.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.Out.WriteLine("Error on opening connection: " + ex.Message);
+                }
+
+                foreach (var query in root.queries)
+                {
+
+                    try
+                    {
+                        using (var command = new NpgsqlCommand(query, conn))
+                        {
+                            command.ExecuteNonQuery();
+                            Console.Out.WriteLine("Finished executing command: {0}/{1}", count, root.queries.Count);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Out.WriteLine("Error executing command: {0}/{1}", count, root.queries.Count);
+                    }
+                    count++;
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
---
+++ jsonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PostgreQueryRunner
+{
+    public class Appsettings
+    {
+        public string host { get; set; }
+        public string port { get; set; }
+        public string dbname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Root
+    {
+        public Appsettings appsettings { get; set; }
+        public List<string> queries { get; set; }
+    }
+}
